8dafa39d397062a258603faafb9937dc253d3c17
Andrea Stagi <stagi.andrea@gmail.com>
2020-03-31 17:11:11 +0200

    Add attributes for image

--- src/Piuma.vue
+++ src/Piuma.vue
@@ -1,5 +1,5 @@
 <template>
-  <img :src="url" />
+  <img :src="url" :title="title" :alt="alt" :class="customclass" />
 </template>
 
 <script lang="ts">
@@ -8,6 +8,9 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class Piuma extends Vue {
   @Prop({ required: true }) readonly src!: string;
+  @Prop({ required: false, default: '' }) readonly customclass!: string;
+  @Prop({ required: false, default: '' }) readonly title!: string;
+  @Prop({ required: false, default: '' }) readonly alt!: string;
   @Prop({ required: false, default: 100 }) readonly quality!: number;
   @Prop({ required: false, default: 0 }) readonly height!: number;
   @Prop({ required: false, default: 0 }) readonly width!: number;
--- tests/unit/piuma.spec.ts
+++ tests/unit/piuma.spec.ts
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import Piuma from '@/Piuma.vue'
 
 describe('ImagePlaceholder.vue', () => {
-  it('renders the correct url for an image images', () => {
+  it('renders the correct url for an image', () => {
     (Piuma as any).serverUrl = "https://loremflickr.com/"
     const wrapper = shallowMount(Piuma, {
       propsData: { width: 500, height: 200, src: 'https://io.io/piuma.png' }
@@ -10,5 +10,27 @@ describe('ImagePlaceholder.vue', () => {
     expect(
       wrapper.findAll('img').at(0).attributes().src
     ).toEqual('https://loremflickr.com/500_200_100/https://io.io/piuma.png')
+  }),
+  it('renders the correct attributes for an image', () => {
+    (Piuma as any).serverUrl = "https://loremflickr.com/"
+    const wrapper = shallowMount(Piuma, {
+      propsData: {
+        width: 500, height: 200,
+        src: 'https://io.io/piuma.png', class: 'customstyle',
+        alt: 'alternate', title: 'title'
+      }
+    })
+    expect(
+      wrapper.findAll('img').at(0).attributes().src
+    ).toEqual('https://loremflickr.com/500_200_100/https://io.io/piuma.png')
+    expect(
+      wrapper.findAll('img').at(0).attributes().class
+    ).toEqual('customstyle')
+    expect(
+      wrapper.findAll('img').at(0).attributes().alt
+    ).toEqual('alternate')
+    expect(
+      wrapper.findAll('img').at(0).attributes().title
+    ).toEqual('title')
   })
 })
